51ce9cb81644536856133b5724e3398321f86e8a
shangsunset <shangsunset@gmail.com>
2016-03-28 23:24:10 -0400

    handle user input

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+'use strict';
+
 const exec = require('child_process').exec;
+const fs = require('fs');
+
 const fetch = require('node-fetch');
 const Promise = require('promise');
+const argv = require('minimist')(process.argv.slice(2));
+const read = require('read');
+
+
 
 const username = '';
 const password = '';
@@ -16,6 +24,51 @@ const data = {
   'has_downloads': true
 };
 
+class Gittohub {
+
+  constructor() {
+    this.username = '';
+    this.password = '';
+    // this.projectName = projectName;
+    // this.projectDescription = projectDescription;
+  }
+
+  getUserInfo() {
+
+    const configPath = `${process.env.HOME}/.gittohub`;
+
+    return new Promise((resolve, reject) => {
+
+      fs.stat(configPath, (error, stats) => {
+        if (error) {
+
+          getCredentials((username, password) => {
+
+            const credentials = {username, password};
+            fs.writeFileSync(configPath, JSON.stringify(credentials), 'utf-8');
+            resolve(JSON.stringify(credentials));
+
+          });
+
+        } else {
+
+          fs.readFile(configPath, (error, data) => {
+            if (error) reject(error);
+
+            resolve(data);
+          });
+        }
+
+
+      });
+    });
+  }
+
+
+
+}
+
+
 function makeRequest(data) {
 
   return fetch(url, {
@@ -27,7 +80,6 @@ function makeRequest(data) {
   })
   .then((res) => {
 
-    console.log(res);
     if (res.status === 201) {
       console.log('created');
     } else if (res.status === 442) {
@@ -77,14 +129,26 @@ function initGit(path) {
   });
 }
 
-makeDir('test').then(res => {
-  
-  const path = `${__dirname}/${res.newDir}`
-  return initGit(path);
+// makeDir('test').then(res => {
+//   
+//   const path = `${__dirname}/${res.newDir}`
+//   return initGit(path);
+//
+// }).then(res => {
+//   console.log(res);
+// })
+// .catch(error => {
+//   console.log(error);
+// })
 
-}).then(res => {
-  console.log(res);
-})
-.catch(error => {
-  console.log(error);
-})
+const project = new Gittohub();
+project.getUserInfo().then(res => console.log(res)).catch(error => console.log(error))
+
+function getCredentials(cb) {
+ 
+  read({prompt: 'github username: '}, (error, username) => {
+    read({prompt: 'github password: ', silent: true}, (error, password) => {
+      cb(username, password)
+    });
+  });
+}
